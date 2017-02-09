96d752f75129607114933bd7689c02dee1d8ffef
MehdiHennaoui <medh31@gmail.com>
2017-02-09 12:16:12 +0100

    create headingClick function in app.js

--- imports/client/app.js
+++ imports/client/app.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 
+let hello = 'Scoot';
+
+//let headingClick = function() {
+
+//	console.log('hello');
+
+//}
+
 export default class App extends Component {
+
+	headingClick() {
+
+		console.log('hello');
+
+	}
 
 	render() {
 
 		return (
 			
-			<h1>Hello !</h1>
+			<h1 onClick={this.headingClick}>Hello {hello}!</h1>
 
 		);
 
