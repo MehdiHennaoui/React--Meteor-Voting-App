ea0efd363e8a5a303af45b91d238dc8d41192501
MehdiHennaoui <medh31@gmail.com>
2017-02-17 10:24:58 +0100

    Add form events and collection inserting

--- imports/client/app.js
+++ imports/client/app.js
@@ -5,7 +5,30 @@ import	Item from './Item';
 import Items from '../api/Items';
 
 class App extends Component {
+	addItems(event){
 
+		event.preventDefault();
+		const itemOne = this.refs.itemOne.value.trim();
+		const itemTwo = this.refs.itemTwo.value.trim();
+
+
+		if(itemOne != '' && itemTwo != '') {
+
+		console.log(this.refs.itemOne.value.trim());
+		 Items.insert({
+    		itemOne: {
+      			text: itemOne,
+      			value: 0,
+    		},
+    
+    		itemTwo: {
+      			text: itemTwo,
+      			value: 0,
+    		}
+  			});
+		} 
+
+	}
 	render() {
 
 		return (
@@ -17,8 +40,13 @@ class App extends Component {
 				</header>
 
 				<main>
+				<form onSubmit={this.addItems.bind(this)}>
+					<input type="text" ref="itemOne"/>
+					<input type="text" ref="itemTwo"/>
+					<button type="submit">Add Items</button>
+				</form>
 					{this.props.items.map((item) => {
-						return <Item item={item}/>
+						return <Item item={item} key={item._id}/>
 					})}
 				</main>
 			</div>
--- server/main.js
+++ server/main.js
@@ -5,16 +5,6 @@ import Items from '../imports/api/Items';
 
 Meteor.startup(() => {
   
-  Items.insert({
-    itemOne: {
-      text: 'Hi',
-      value: 0,
-    },
-    
-    itemTwo: {
-      text: 'Hello',
-      value: 0,
-    }
-  });
+ 
   
 });
