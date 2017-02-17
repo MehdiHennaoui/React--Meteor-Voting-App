3566b0ef79f5c9835a107a12ae2f8618403c9cb7
MehdiHennaoui <medh31@gmail.com>
2017-02-17 14:27:40 +0100

    Updating and save of votes

--- imports/client/Item.js
+++ imports/client/Item.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import Items from '../api/Items';
 
 export default class Item extends Component {
+	
+	voteOne(){
+
+		Items.update(this.props.item._id,{
+			$inc: {
+				'itemOne.value':1
+			}
+		})
+
+	}
+
+	voteTwo(){
+
+		Items.update(this.props.item._id,{
+			$inc: {
+				'itemTwo.value':1
+			}
+		})
+
+	}
+
 
 	render() {
 
 		return(
 
 				<div className='item'>
-					<div className='vote-one'>
+					<div className='vote-one' onClick={this.voteOne.bind(this)}>
 						
 						<span>{this.props.item.itemOne.value}</span>
 						<h3>{this.props.item.itemOne.text}</h3>
@@ -16,7 +38,7 @@ export default class Item extends Component {
 						
 						<span>vs</span>
 					
-					<div className='vote-two'>
+					<div className='vote-two' onClick={this.voteTwo.bind(this)}>
 						
 						<span>{this.props.item.itemTwo.value}</span>
 						<h3>{this.props.item.itemTwo.text}</h3>
